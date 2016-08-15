0f40edd1d2becbf1f4695c46d5cb4c8132437936
Nate Sanchez-Goodwin <Nate@south-30-2.wireless.griffith.edu.au>
2016-08-15 15:03:03 +1000

    add more controllers and models

---
+++ database/migrations/2016_08_15_045842_create_promocode_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromocodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promocode', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('number_of_uses');
+            $table->integer('percentage');
+            $table->integer('days');
+            $table->date('expiry');
+            $table->string('code');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('promocode');
+    }
+}
