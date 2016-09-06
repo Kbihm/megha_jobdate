4900a67c5e5900c6dc34f9f3a70e05744b6ac471
Liamso <Liam.southwell@gmail.com>
2016-09-06 12:50:10 +1000

    employer /profile done closes #14 closes #15

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -28,9 +28,9 @@
 
                  <div class="panel-body">
  
-                    <form class="form-horizontal" role="form" method="POST" action="/p/create">
+                    <form class="form-horizontal" role="form" method="POST" action="/p/edit">
                         {{ csrf_field() }}
-                        
+                        {{ method_field('PATCH')}}
                         <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
                             <label for="first_name" class="col-md-4 control-label">First Name:</label>
 
@@ -150,7 +150,7 @@
                         </div>      
 
                         <div class="form-group{{ $errors->has('hourly_rate') ? ' has-error' : '' }}">
-                            <label for="hourly_rate" class="col-md-4 control-label">What is your desired hourly rate? </label>
+                            <label for="hourly_rate" class="col-md-4 control-label">What is your desired abn? </label>
 
                             <div class="col-md-6">
                                 <input id="hourly_rate" type="number" class="form-control" name="hourly_rate" value="{{ $user->employee->hourly_rate }}">
@@ -175,13 +175,132 @@
                     </form>
                 </div>
             @elseif (Auth::user()->employer_id != null)
+                                 <div class= "panel-body text-center">
+                    <h2 style="color:red"> Subscription Ends: {{ $user->employer->subscription_end }} <br/> </h2>
+                    <form class="form-horizontal" role="form" method="POST" action="/p/edit">
+                        {{ csrf_field() }}
+                        {{ method_field('PATCH')}}
+                        <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
+                            <label for="first_name" class="col-md-4 control-label">First Name:</label>
 
-                Account Type: Employer <br/>
-                Phone: {{ $user->employer->phone }} <br/>
-                ABN: {{ $user->employer->abn }} <br/>
-                Address: {{ $user->employer->address }} <br/>
-                Establishment name: {{ $user->employer->establishment_name }} <br/>
-                Date: {{ $user->employer->subscription_end }} <br/>
+                            <div class="col-md-6">
+                                <input id="first_name" type="text" class="form-control" name="first_name" value="{{ $user->first_name }}">
+
+                                @if ($errors->has('first_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('first_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
+                            <label for="last_name" class="col-md-4 control-label">Last Name:</label>
+
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control" name="last_name" value="{{ $user->last_name }}">
+
+                                @if ($errors->has('last_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('last_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                                                
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-mail:</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $user->email }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div> 
+
+                        <div class="form-group{{ $errors->has('email_confirm') ? ' has-error' : '' }}">
+                            <label for="email_confirm" class="col-md-4 control-label">Confirm E-mail:</label>
+
+                            <div class="col-md-6">
+                                <input id="email_confirm" type="text" class="form-control" name="email_confirm" value="{{ $user->email }}">
+
+                                @if ($errors->has('email_confirm'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email_confirm') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>         
+
+                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            <label for="phone" class="col-md-4 control-label">Phone Number:</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" type="text" class="form-control" name="phone" value="{{ $user->employer->phone }}">
+
+                                @if ($errors->has('phone'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div> 
+
+                        <div class="form-group{{ $errors->has('abn') ? ' has-error' : '' }}">
+                            <label for="abn" class="col-md-4 control-label">What is your abn? </label>
+
+                            <div class="col-md-6">
+                                <input id="abn" type="number" class="form-control" name="abn" value="{{ $user->employer->abn }}">
+
+                                @if ($errors->has('abn'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('abn') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                        <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                            <label for="address" class="col-md-4 control-label">What is your address? </label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control" name="address" value="{{ $user->employer->address }}">
+
+                                @if ($errors->has('address'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('establishment_name') ? ' has-error' : '' }}">
+                            <label for="establishment_name" class="col-md-4 control-label">What is your establishment_name? </label>
+
+                            <div class="col-md-6">
+                                <input id="establishment_name" type="text" class="form-control" name="establishment_name" value="{{ $user->employer->establishment_name }}">
+
+                                @if ($errors->has('establishment_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('establishment_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-success">
+                                     Edit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
 
             @else
                 No role set, Uh oh... 
