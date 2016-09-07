5a302edb2822afc75ab5e3f98a4d9d2f9765bdc0
Liamso <Liam.southwell@gmail.com>
2016-09-07 20:54:43 +1000

    fixed user index html

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,47 +2,48 @@
 @section('content')
 
     <div class="col-md-10 col-md-offset-2">
-
+        
                     @foreach($users as $user)
                         @if($user->employee_id != null)
 
-
                         <div class="panel panel-default">
-                            <div class="panel panel-heading">
-                                {{$user->first_name}} {{$user->last_name}}
-
-                                <div class="col-md-7 pull-right">
-                                    <div class="progress">
-                                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
-                                        aria-valuemin="0" aria-valuemax="100" style="width:<?php $rating = $user->employee->average_rating; echo $rating*10; ?>%">
-                                            
+                            <div class="panel-heading">
+                                <div class="row">
+                                <div class="col-sm-4">
+                                        <h4>{{$user->first_name}} {{$user->last_name}}</h4> 
+                                </div>
+                                    <div class="col-sm-6 pull-right">
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
+                                            aria-valuemin="0" aria-valuemax="100" style="width:<?php $rating = $user->employee->average_rating; echo $rating*10; ?>%"> 
+                                            </div>
+                                            <div class="col-md-4"> Average Rating:  <?php $rating = $user->employee->average_rating; echo $rating*10; ?>% </div>
                                         </div>
-                                        <div class="col-md-4"> Average Rating:  <?php $rating = $user->employee->average_rating; echo $rating*10; ?>% </div>
-                                    </div>
-                                </div>   
+                                    </div>   
+                                </div>
                             </div>
-                            <div class="panel panel-body">
+                            <div class="panel-body">
                                    <div class="col-md-3">
-                                   <img src="http://tinyurl.com/javaltj"></img>
+                                        <img src="http://tinyurl.com/javaltj"></img>
                                    </div>
                                    <div class="col-md-3">
-                                   <strong>Hourly Rate: {{$user->employee->hourly_rate}}</strong>
+                                        <strong>Hourly Rate: {{$user->employee->hourly_rate}}</strong>
                                    </div>
                                    <div class="col-md-3">
-                                   <strong>Bio:</strong> {{ str_limit($user->employee->about, $limit = 100, $end = '...') }}
+                                        <strong>Bio:</strong> {{ str_limit($user->employee->about, $limit = 100, $end = '...') }}
                                    </div>
                                    <div class="col-md-3">
-                                    <strong> availability </strong>
-                                    <button class="btn"> Send Job Offer </button>
+                                        <strong> availability </strong>
+                                        <button class="btn"> Send Job Offer </button>
                                    </div>
                             </div>
-                           (how can i remove this extra panel line?)
-                        </div>
+                            </div>
+
 
 
                         @endif
                     @endforeach
-
+        
     </div>
 
 @endsection           
