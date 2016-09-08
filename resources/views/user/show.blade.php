3df0c67f58208be2453a4db04222e5d6735f25d8
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-09-09 09:46:41 +1000

    clean indiv comments view

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -39,17 +39,49 @@
                 </div>
             </div>
 
-            <!-- 
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Skills</h3>
+            </div>
+            <div class="panel-body">
+                @if (sizeof($user->employee->skill) > 0)
+                <ul>
+                    @foreach ($user->employee->skill as $skill)
+                    <li> {{ $skill->name }} </li>
+                    @endforeach
+                </ul>
+                @else
+                    <p> {{ $user->first_name }} hasn't put in any skills just yet. </p>
+                @endif
+            </div>
+        </div>
 
-                <div class="col-md-6" style="height:200px;">
-                    <div class="list-group">
-                        <a href="#" class="list-group-item active">
-                            Comments <span class="badge"><?php $number = count($comments); echo $number; ?></span>
-                        </a>
-                        <div class="list-group-item">
-                            <div class="pre-scrollable" style="height: 125px;">
+        
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Experience</h3>
+            </div>
+            <div class="panel-body">
+                @if (sizeof($user->employee->experience) > 0)
+                    @foreach ($user->employee->experience as $experience)
+                    <h4>{{ $experience->title }}  <span class="text-muted"> ({{ $experience->establishment_name}})</span> </h4>
+                    <h6> {{ $experience->employment_length }} </h6>
+                    <p> {{ $experience->description }} </p>
+                    <hr>
+                    @endforeach
+                @else
+                    <p> {{ $user->first_name }} hasn't put in any experience just yet. </p>
+                @endif
+            </div>
+        </div>
 
-                            @foreach($comments as $comment)
+
+        <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Reviews about {{$user->first_name}} from other Employers</h3>
+                </div>
+                <div class="panel-body">
+                    @foreach($comments as $comment)
                                 <div class="list-group-item">
                                     <div class="col-sm-2 pull-right">
                                         <icon class="btn-sm btn-<?php 
@@ -73,42 +105,9 @@
                                         </form>
                                     @endif
                                 </div>
-                            @endforeach
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div> -->
-
-
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Skills</h3>
-            </div>
-            <div class="panel-body">
-                <ul>
-                    @foreach ($user->employee->skill as $skill)
-                    <li> {{ $skill->name }} </li>
                     @endforeach
-                </ul>
+                </div>
             </div>
-        </div>
-
-        
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">Experience</h3>
-            </div>
-            <div class="panel-body">
-                @foreach ($user->employee->experience as $experience)
-                <h4>{{ $experience->title }}  <span class="text-muted"> ({{ $experience->establishment_name}})</span> </h4>
-                <h6> {{ $experience->employment_length }} </h6>
-                <p> {{ $experience->description }} </p>
-                <hr>
-                @endforeach
-            </div>
-        </div>
 
         </div>
         
