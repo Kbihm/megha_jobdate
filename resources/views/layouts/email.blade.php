dfdf91b054009b986bbf5694b407f6731baf817c
Liamso <Liam.southwell@gmail.com>
2016-09-28 13:33:51 +1000

    fix email html double footer

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -111,13 +111,6 @@
     }
     </style>
   </head>
-            <!-- START FOOTER -->
-            <div class="footer" style="clear: both; padding-top: 10px; text-align: center; width: 100%;">
-              <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
-                <tr>
-                  <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-top: 10px; padding-bottom: 10px; font-size: 12px; color: #999999; text-align: center;" valign="top" align="center">
-                    <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">JobDate, 50 Burleigh Burleigh, <!doctype html>
-<html>
   <head>
     <meta name="viewport" content="width=device-width">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -249,26 +242,6 @@
                   </td>
                 </tr>
                 <tr>
-                </tr>
-              </table>
-            </div>
-
-            <!-- END FOOTER -->
-            
-<!-- END CENTERED WHITE CONTAINER --></div>
-        </td>
-        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">&nbsp;</td>
-      </tr>
-    </table>
-  </body>
-</html>San Francisco CA 94102</span>
-                    <br> Don't like these emails? <a href="http://i.imgur.com/CScmqnj.gif" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">Unsubscribe</a>.
-                  </td>
-                </tr>
-                <tr>
-                  <td class="content-block powered-by" style="font-family: sans-serif; vertical-align: top; padding-top: 10px; padding-bottom: 10px; font-size: 12px; color: #999999; text-align: center;" valign="top" align="center">
-                   
-                  </td>
                 </tr>
               </table>
             </div>
