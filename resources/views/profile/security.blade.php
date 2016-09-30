7cf99bf7d4d95554646ffff7e143521735bf7736
Liamso <Liam.southwell@gmail.com>
2016-09-30 14:31:17 +1000

    closes #42

--- resources/views/profile/security.blade.php
+++ resources/views/profile/security.blade.php
@@ -10,7 +10,7 @@
             <li><a href="/profile">Your Information</a></li>
             @if ($user->employee_id != null)
             <li class=""><a href="/profile/skills" >Skills</a></li>
-            <li class=""><a href="experience">Experience</a></li>
+            <li class=""><a href="/experience">Experience</a></li>
             @endif
             @if ($user->employer_id != null)
             <li class=""><a href="/profile/subscription" >Subscription</a></li>
@@ -30,7 +30,7 @@
         -->
         <div class="tab-pane fade active in" id="security">
            
-          <div class="col-md-8 col-md-offset-2">
+          <div class="col-md-10">
             <div class="panel panel-default">
                 <div class="panel-heading">Change Password</div>
                 <div class="panel-body">
@@ -84,16 +84,97 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-promocode"></i> Edit
+                                     Edit
                                 </button>
                             </div>
                         </div>
                     </form>
                     </div>
+
+
                 </div>
             </div>
         </div>
 
+         <div class="col-md-10">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Card Details</div>
+                <div class="panel-body">
+ 
+                    <form class="form-horizontal" role="form" method="POST" action="">
+                        {{ csrf_field() }}
+                        
+                        <div class="form-group{{ $errors->has('number') ? ' has-error' : '' }}">
+                            <label for="number" class="col-md-4 control-label">Card Number:</label>
+
+                            <div class="col-md-6">
+                                <input id="number" type="text" class="form-control" name="number" value="{{ old('number') }}">
+
+                                @if ($errors->has('number'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('number') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                                                
+                        <div class="form-group{{ $errors->has('expiry') ? ' has-error' : '' }}">
+                            <label for="expiry" class="col-md-4 control-label">Expiry:</label>
+
+                            <div class="col-md-6">
+                                <input id="expiry" type="textarea" class="form-control" name="expiry" value="{{ old('expiry') }}">
+
+                                @if ($errors->has('expiry'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('expiry') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name as it appears on card:</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="textarea" class="form-control" name="name" value="{{ old('name') }}">
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>  
+
+                        <div class="form-group{{ $errors->has('ccv') ? ' has-error' : '' }}">
+                            <label for="ccv" class="col-md-4 control-label">CCV:</label>
+
+                            <div class="col-md-6">
+                                <input id="ccv" type="textarea" class="form-control" ccv="ccv" value="{{ old('ccv') }}">
+
+                                @if ($errors->has('ccv'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('ccv') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>  
+                        
+                           
+                        
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                     Update
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+        </div>
+    
     </div>
 
 @endsection           
