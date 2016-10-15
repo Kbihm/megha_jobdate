e912cccdd02da12c3c089acd93d95068ae798b93
Liamso <asleep.and.hairy@gmail.com>
2016-10-15 15:27:17 +1000

    a

--- app/Http/Controllers/EmailsController.php
+++ app/Http/Controllers/EmailsController.php
@@ -27,7 +27,54 @@ class EmailsController extends Controller
     return "Your email has been sent successfully";
     }
         
-   
+        //Any time a dispute is filed through the dispute system
+    public function dispute() 
+    {
 
+    }
+        //When a new user signs signs Up
+    public function signUp() 
+    {
 
+    }
+        // When a user hasn't logged in for 7 days after a job request
+    public function noLogin() 
+    {
+
+    }
+        // When an employer requests the details of an employee
+    public function requestDetails() 
+    {
+
+    }
+        // Acceptance of a job
+        public function acceptJob() 
+    {
+
+    }
+        // Declination of a job
+        public function declineJob() 
+    {
+
+    }
+        // Confirmation of a job
+        public function confirmJob() 
+    {
+
+    }
+        // Reminding an employer to renew their sub
+        public function renewSub() 
+    {
+
+    }
+        // Reminding and employer to review and employee
+        public function reviewRemind() 
+    {
+
+    }
+        // When a job request is sent
+        public function sendJobRequest() 
+    {
+
+    }
 }
