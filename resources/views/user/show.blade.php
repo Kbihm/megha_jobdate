6f736e2060d65259c68a93e40b7607a58a82d58d
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-11-14 18:02:30 +1000

    calendar changes

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -41,11 +41,11 @@
                 /*background-color: #dedede;*/
                 }
                 #row{
-                    clear: left;
-                    float: left;
-                    height: 25%;
-                    width: 100%;
-
+                    height: 22%;
+                    width: 96%;
+                    margin-left: 2%;
+                    margin-right: 2%;
+                    margin-bottom: 2%;
                 }
                 .calendar .day,
                 .calendar .inactive {
