4676b098cd09b82f0aea93daf78e71d0b6eeabb1
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-11-15 12:17:32 +1000

    custom 404 page closes #3

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-8">
+
+            <div class="jumbotron">
+                <h1>Whoops! - 404 Error</h1>
+                <p>That Page Doesn't exist.</p>
+                <a href="/" class="btn btn-success btn-fill">Home Page</a>
+            </div>
+
+        </div>
+
+        </div>
+    </div>
+</div>
+
+
+@endsection
