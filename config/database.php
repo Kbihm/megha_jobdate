10958f09e4dda5866220bad9c4afca4160a05c95
Nate Sanchez-Goodwin <nsg223@gmail.com>
2017-01-12 20:33:50 +1000

    travis

--- config/database.php
+++ config/database.php
@@ -78,6 +78,18 @@ return [
             'schema' => 'public',
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', '127.0.0.1'),
+            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username'  => env('DB_TEST_USERNAME', 'root'),
+            'password'  => env('DB_TEST_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
     ],
 
     /*
