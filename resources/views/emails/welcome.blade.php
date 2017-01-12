5f17793067b1aeaf1ba928f82b3984048c7b8f88
Liamso <asleep.and.hairy@gmail.com>
2017-01-12 21:01:54 +1000

    E-mails-Fix (#193)

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -8,7 +8,7 @@
                     <tr>
                       <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">
                         <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Congratulations, {{$user->first_name}} <br></p>
-                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Welcome to job date, your new era of employment hiring, where your in charge of the hiring process from the start, simply, choose your desired candidate and offer them the job, no more unwanted phine calls and emails from unqualified applicantdspicking the right candidate for the right time to suit your business best, your nearly there just follow the link below to verify your account and start searching today for potential employees. </p> 
+                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;"> welcome to job date, your new era of employment hiring, where you are in charge of the hiring process from the start, simply, choose your desired candidate and offer them the job, no more unwanted phone calls and emails from unqualified applicants, picking the right candidate for the right time to suit your business best, your nearly there just follow the link below to verify your account and start searching today for potential employees. </p> 
                       </td>
                     </tr>
                   </table>
