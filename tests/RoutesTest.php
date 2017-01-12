0e618f5b487ac521c141042d3c80f6407dbdc174
Nate Sanchez-Goodwin <nsg223@gmail.com>
2017-01-12 19:41:34 +1000

    fix tests

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -17,10 +17,10 @@ class RoutesTest extends TestCase
      */
     public function testPublicRoutes()
     {
-        $this->visit('/register');
-        $this->visit('/staff');
-        $this->visit('/register/employer');
-        $this->visit('/register/employee');
+        $this->visit('/register')->seeStatusCode(200);
+        $this->visit('/staff')->seeStatusCode(200);
+        $this->visit('/register/employer')->seeStatusCode(200);
+        $this->visit('/register/employee')->seeStatusCode(200);
     }
 
     public function testAdminRoutes()
@@ -33,15 +33,15 @@ class RoutesTest extends TestCase
         $this->visit('/home')
             ->see('Admin');
 
-        $this->visit('/admin/user');
-        $this->visit('/admin/user/1');
-        $this->visit('/staff');
-        $this->visit('/admin/comments');
-        $this->visit('/admin/settings');
+        $this->visit('/admin/user')->seeStatusCode(200);
+        $this->visit('/admin/user/1')->seeStatusCode(200);
+        $this->visit('/staff')->seeStatusCode(200);
+        $this->visit('/admin/comments')->seeStatusCode(200);
+        $this->visit('/admin/settings')->seeStatusCode(200);
 
-        $this->visit('/profile');
-        $this->visit('/profile/security');
-        $this->visit('/profile/delete');
+        $this->visit('/profile')->seeStatusCode(200);
+        $this->visit('/profile/security')->seeStatusCode(200);
+        $this->visit('/profile/delete')->seeStatusCode(200);
     }
 
     public function testEmployeeRoutes() {
@@ -50,18 +50,18 @@ class RoutesTest extends TestCase
 
         $this->be($user);
 
-        $this->visit('/offers');        
-        $this->visit('/my-reviews');
-        $this->visit('/home');
+        $this->visit('/offers')->seeStatusCode(200);
+        $this->visit('/my-reviews')->seeStatusCode(200);
+        $this->visit('/home')->seeStatusCode(200);
 
-        $this->visit('/staff/'.$user->id);
+        $this->visit('/staff/1')->seeStatusCode(200);
 
-        $this->visit('/profile');
-        $this->visit('/profile/skills');
-        $this->visit('/profile/experience');
-        $this->visit('/profile/availability');
-        $this->visit('/profile/security');
-        $this->visit('/profile/delete');
+        $this->visit('/profile')->seeStatusCode(200);
+        $this->visit('/profile/skills')->seeStatusCode(200);
+        $this->visit('/profile/experience')->seeStatusCode(200);
+        $this->visit('/profile/availability')->seeStatusCode(200);
+        $this->visit('/profile/security')->seeStatusCode(200);
+        $this->visit('/profile/delete')->seeStatusCode(200);
     }
 
     public function testEmployerRoutes() {
@@ -70,15 +70,16 @@ class RoutesTest extends TestCase
 
         $this->be($user);
 
-        $this->visit('/jobs');        
-        $this->visit('/reviews');
-        $this->visit('/staff');
-        $this->visit('/staff/1');
-        $this->visit('/home');
+        $this->visit('/jobs')->seeStatusCode(200);        
+        $this->visit('/reviews')->seeStatusCode(200);
+        $this->visit('/staff')->seeStatusCode(200);
+        $this->visit('/staff/1')->seeStatusCode(200);
+        $this->visit('/home')->seeStatusCode(200);
 
-        $this->visit('/profile');
-        $this->visit('/profile/subscription');
-        $this->visit('/profile/security');
-        $this->visit('/profile/delete');
+        $this->visit('/profile')->seeStatusCode(200);
+        $this->visit('/profile/subscription')->seeStatusCode(200);
+        $this->visit('/profile/security')->seeStatusCode(200);
+        $this->visit('/profile/delete')->seeStatusCode(200);
     }
+
 }
