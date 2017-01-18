26e27dbc4d5697414a8dd8377843b1b6f5230466
Liamso <asleep.and.hairy@gmail.com>
2017-01-18 12:47:26 +1000

    Fixed dispute emails

--- resources/views/emails/sendDispute.blade.php
+++ resources/views/emails/sendDispute.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 
 {{ $user->email }} has filed a dispute about the comment with the text: {{ $comment->description }}
- <a href="http://app.jobdate.com.au/staff/{{$user->employee->id}}"> Click here to view profile. </a>
+ <a href="https://app.jobdate.com.au/staff/{{$user->employee->id}}"> Click here to view profile. </a>
                 
 @endsection
