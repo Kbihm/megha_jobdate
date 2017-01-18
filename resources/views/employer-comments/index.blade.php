505aeb0abfed527fe558f1171801e243850fb9c2
Liamso <asleep.and.hairy@gmail.com>
2017-01-18 13:40:44 +1000

    fix display of comments rating closes#221, closes#220

--- resources/views/employer-comments/index.blade.php
+++ resources/views/employer-comments/index.blade.php
@@ -54,11 +54,11 @@ Reviews
                                     @if ($comment-> approved == 0)
                                         <span class="label label-default">Pending Admin Approval</span>
                                     @endif
-                                    @if ($comment->rating == 1)
+                                    @if ($comment->rating == 0)
                                         <span class="label label-danger">Negative</span>
-                                    @elseif ($comment->rating == 2)
+                                    @elseif ($comment->rating == 1)
                                         <span class="label label-default">Neutral</span>
-                                    @elseif ($comment->rating == 3)
+                                    @elseif ($comment->rating == 2)
                                         <span class="label label-success">Positive</span>
                                     @endif
                                     </div>
