8e506b87606dc8c4680d032cc983b135bf09514a
Liamso <asleep.and.hairy@gmail.com>
2017-01-20 13:27:13 +1000

    Update Employer.php

--- app/Http/Middleware/Employer.php
+++ app/Http/Middleware/Employer.php
@@ -24,6 +24,7 @@ class Employer
         
         }
         elseif ($user->employer_id != null) {
+            
             if ($user->subscribed('main')) {
                 return $next($request);
             }
