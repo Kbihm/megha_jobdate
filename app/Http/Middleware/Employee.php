4dd34c4ca83e812f31f4e054be278a0c4d4ede7b
Liamso <asleep.and.hairy@gmail.com>
2017-01-25 12:09:25 +1000

    closes #231 changed button on email
    also changed redirect to login page if auth::check fails in middleware

--- app/Http/Middleware/Employee.php
+++ app/Http/Middleware/Employee.php
@@ -16,6 +16,9 @@ class Employee
      */
     public function handle($request, Closure $next)
     {
+        if(!Auth::Check()){
+            return view('welcome');
+        }
         if (Auth::user()->employee->email_confirmed == false)
             return back()->with("error", "You need to confirm your email before accessing that page. Please check your email account and click the link to verify your email.
              Ensure you check your spam folder and if you still can't find the email contact support.");
