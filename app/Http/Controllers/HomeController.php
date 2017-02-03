61a0691eddab2c8e9dbba64f763fc99c94af7d26
Nate Sanchez-Goodwin <nsg223@gmail.com>
2017-02-03 22:28:31 +1000

    changes around orders of reviews

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,8 +38,7 @@ class HomeController extends Controller
         }
         if(Auth::User()->employee_id != null){
 
-        $review = Comment::where('employee_id', '=', Auth::User()->employee->id)->first();
-
+        $review = Comment::where('employee_id', '=', Auth::User()->employee->id)->orderBy("updated_at", "asc")->first();
 
         return view('home', compact('review'));
         }
